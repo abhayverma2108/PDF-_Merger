578708daa09abec4268266bdd78bbac169fb6733
Abhay Verma <75964286+abhayverma2108@users.noreply.github.com>
2024-04-10 01:57:56 +0300

    main code

--- PDF_Merger/main.py
+++ PDF_Merger/main.py
@@ -1,1 +1,9 @@
-# this is my first test comment
+import PyPDF2
+import os
+
+merger = PyPDF2.PdfMerger()
+
+for file in os.listdir(os.curdir):
+    if file.endswith(".pdf"):
+        merger.append(file)
+merger.write("merged.pdf")
